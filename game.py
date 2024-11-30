83297c453d646505937f3ae55ae2ba303f719f1a
Rick <henriqueabatalha@gmail.com>
2024-11-30 11:18:20 -0300

    Adiciona contador de movimentos e melhora gameover

--- game.py
+++ game.py
@@ -43,20 +43,21 @@ class Grafo:
 class Player:
     def __init__(self, grafo, no_inicial): # Recebe grafo e no inical
         self.grafo = grafo
-        self.no_atual = no_inicial
+        self.no_atual = no_inicial # Guarda no atual
         self.posicao = grafo.get_posicao(no_inicial)
     
     def mover(self, direção):
         arestas = self.grafo.get_arestas(self.no_atual)
         if direção in arestas: # Se direcao da aresta esta conectada
-            self.no_atual = direção
+            self.no_atual = direção # Guarda as coordenadas [X,Y] atuais
             self.posicao = self.grafo.get_posicao(direção) # Atualiza posicao
 
+# Classe do Inimigo
 class Inimigo:
     def __init__(self, grafo, no_inicial): # Recebe grafo e no inical
         self.grafo = grafo
-        self.no_atual = no_inicial
-        self.posicao = grafo.get_posicao(no_inicial)
+        self.no_atual = no_inicial # Guarda no atual
+        self.posicao = grafo.get_posicao(no_inicial) # Guarda as coordenadas [X,Y] atuais
     
     def mover(self, direção): # TROCAR PARA DIJISKTRA
         arestas = self.grafo.get_arestas(self.no_atual)
@@ -121,39 +122,71 @@ def desenhar_player():
 def desenhar_inimigo():
     pygame.draw.circle(screen, VERMELHO, inimigo.posicao, TAMANHO_NO // 2) # Faz ele um pouc menor que o no
 
+# Funcao para desenhar o gameover
+def desenhar_gameover():
+    font = pygame.font.Font('freesansbold.ttf', 50) # Seleciona a fonte 
+    screen.fill(PRETO) # Pinta o fundo todo de preto
+    text = font.render('GAMEOVER', True, PRETO, AZUL) # Escreve GAMEOVER em azul
+    textRect = text.get_rect()
+    textRect.center = (screen_width // 2, screen_height // 2)
+    screen.blit(text, textRect)
+
+# Funcao para desenhar o contador de movimentos
+def desenhar_contador(movimentos):
+    font = pygame.font.Font('freesansbold.ttf', 12) # Seleciona a fonte 
+    texto = 'Movimentos {}/5'.format(movimentos)  # Formata string 
+    text = font.render(texto, True, PRETO, VERDE)
+    textRect = text.get_rect()
+    textRect.center = (screen_width - 50, 10)
+    screen.blit(text, textRect)
+
 # Funcao principal do jogo
 def game_loop():
     running = True
     jogando = True # Determina se o jogo continua
-    while running:
+    movimentos = 0 # Vai guardar o numero de casas andadas
+    
+
+    while running: # Inicia o loop de jogo
         screen.fill(PRETO) # pinta o fundo
         desenhar_mapa()
         desenhar_player()
         desenhar_inimigo()
+        desenhar_contador(movimentos)
+
+        if movimentos == 5:
+            movimentos = 0 # Reseta o contador de movimentos
+            for vizinho in grafo.get_arestas(inimigo.no_atual):
+                if grafo.get_posicao(vizinho)[1] < inimigo.posicao[1]: # Se Y do vizinho for menor, entao vizinho esta em cima
+                    inimigo.mover(vizinho)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.KEYDOWN and jogando: # Usuario aperta alguma tecla e o player nao encostou em um inimigo
-                if event.key == pygame.K_UP: # Apertou cima
+            elif event.type == pygame.KEYDOWN: # Usuario aperta alguma tecla
+                if event.key == pygame.K_UP and jogando: # Apertou cima
                     for vizinho in grafo.get_arestas(player.no_atual):
                         if grafo.get_posicao(vizinho)[1] < player.posicao[1]: # Se Y do vizinho for menor, entao vizinho esta em cima
+                            movimentos +=1 # Incrementa o contador de movimentos
                             player.mover(vizinho)
                             break
-                elif event.key == pygame.K_DOWN:  # Apertou baixo
+                elif event.key == pygame.K_DOWN and jogando:  # Apertou baixo
                     for vizinho in grafo.get_arestas(player.no_atual):
                         if grafo.get_posicao(vizinho)[1] > player.posicao[1]: # Se Y do vizinho for maior, entao vizinho esta em baixo
                             player.mover(vizinho)
+                            movimentos +=1 # Incrementa o contador de movimentos
                             break
-                elif event.key == pygame.K_RIGHT:  # Apertou direita
+                elif event.key == pygame.K_RIGHT and jogando:  # Apertou direita
                     for vizinho in grafo.get_arestas(player.no_atual):
                         if grafo.get_posicao(vizinho)[0] > player.posicao[0]: # Se X do vizinho for maior, entao vizinho esta a direita
                             player.mover(vizinho)
+                            movimentos +=1 # Incrementa o contador de movimentos
                             break
-                elif event.key == pygame.K_LEFT: #  # Apertou esquerda
+                elif event.key == pygame.K_LEFT and jogando: #  # Apertou esquerda
                     for vizinho in grafo.get_arestas(player.no_atual): # Se X do vizinho for menor, entao vizinho esta a esquerda
                         if grafo.get_posicao(vizinho)[0] < player.posicao[0]:
                             player.mover(vizinho)
+                            movimentos +=1 # Incrementa o contador de movimentos
                             break
                 elif event.key == pygame.K_ESCAPE: # Apertou Esc
                     pygame.quit() #Fecha jogo e programa
@@ -161,12 +194,7 @@ def game_loop():
 
         if player.no_atual == inimigo.no_atual: # Quando o player encosta no inimigo
             jogando = False # Finaliza jogo
-            screen.fill(PRETO) # Pinta o fundo todo de preto
-            font = pygame.font.Font('freesansbold.ttf', 50) # Seleciona a fonte
-            text = font.render('GAMEOVER', True, PRETO, AZUL) # Escreve GAMEOVER em azul
-            textRect = text.get_rect()
-            textRect.center = (screen_width // 2, screen_height // 2)
-            screen.blit(text, textRect)
+            desenhar_gameover()
             
         
         pygame.display.flip()
